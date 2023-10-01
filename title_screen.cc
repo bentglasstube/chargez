0c0eb4589bf464b1146927dca22658e9c08ef2c2
Alan Berndt <alan@eatabrick.org>
2023-10-01 14:57:35 -0700

    Add character descriptions to the title screen

--- title_screen.cc
+++ title_screen.cc
@@ -8,7 +8,8 @@
 TitleScreen::TitleScreen()
     : text_("text.png"),
       space_(Util::random_seed()),
-      title_("title.png", 0, 0, 518, 177) {}
+      title_("title.png", 0, 0, 518, 177),
+      sprites_("sprites.png", 8, 32, 32) {}
 
 bool TitleScreen::update(const Input& input, Audio& audio,
                          unsigned int elapsed) {
@@ -19,7 +20,7 @@ bool TitleScreen::update(const Input& input, Audio& audio,
     if (fade_timer_ > 1000) return false;
   } else {
     timer_ += elapsed;
-    if (timer_ > 1000) timer_ -= 1000;
+    if (timer_ > 1380) timer_ -= 1380;
     if (input.key_pressed(Input::Button::Start)) {
       if (!lets_go) {
         lets_go = true;
@@ -28,6 +29,12 @@ bool TitleScreen::update(const Input& input, Audio& audio,
     }
   }
   title_timer_ += elapsed;
+
+  char_timer_ += elapsed;
+  if (char_timer_ > 10000) {
+    index = (index + 1) % 8;
+    char_timer_ = 0;
+  }
 
   return true;
 }
@@ -43,11 +50,90 @@ void TitleScreen::draw(Graphics& graphics) const {
     graphics.draw_rect({0, 0}, {graphics.width(), graphics.height()}, color,
                        true);
   } else {
-    if (timer_ < 500) {
+    if (timer_ < 690) {
       text_.draw(graphics, "Press start", graphics.width() / 2,
                  graphics.height() - 32, Text::Alignment::Center);
+    }
+
+    if (index > 0) {
+      const int x = graphics.width() / 2;
+      const int y = graphics.height() / 2;
+
+      if (index == 6) {
+        sprites_.draw(graphics, 6, x - 32, y - 32);
+        sprites_.draw(graphics, 7, x, y - 32);
+        sprites_.draw(graphics, 14, x - 32, y);
+        sprites_.draw(graphics, 15, x, y);
+      } else {
+        sprites_.draw(graphics, char_sprite(), x - 16, y - 16);
+      }
+      text_.draw(graphics, char_name(), x, y + 40, Text::Alignment::Center);
+      text_.draw(graphics, char_desc(), x, y + 64, Text::Alignment::Center);
     }
   }
 }
 
 Screen* TitleScreen::next_screen() const { return new GameScreen(); }
+
+int TitleScreen::char_sprite() const {
+  switch (index) {
+    case 1:
+      return 0;
+    case 2:
+      return 4;
+    case 3:
+      return 5;
+    case 4:
+      return 8;
+    case 5:
+      return 24;
+    case 6:
+      return 6;
+    case 7:
+      return 16;
+    default:
+      return 0;
+  }
+}
+
+std::string TitleScreen::char_name() const {
+  switch (index) {
+    case 1:
+      return "Player";
+    case 2:
+      return "Pawn";
+    case 3:
+      return "Dodgy Boi";
+    case 4:
+      return "Kickler";
+    case 5:
+      return "Dizzy Jon";
+    case 6:
+      return "Pusher";
+    case 7:
+      return "Hacker";
+    default:
+      return "";
+  }
+}
+
+std::string TitleScreen::char_desc() const {
+  switch (index) {
+    case 1:
+      return "This is you.  Don't get knocked outside of the ring.";
+    case 2:
+      return "They wander aimlessly.";
+    case 3:
+      return "They will dodge if you try to charge them.";
+    case 4:
+      return "They follow you and try to bump you when they're close.";
+    case 5:
+      return "They will disorient you if you touch them.";
+    case 6:
+      return "They are very strong but they are slow.";
+    case 7:
+      return "Their presence corrupts your view.";
+    default:
+      return "";
+  }
+}
--- title_screen.h
+++ title_screen.h
@@ -3,6 +3,7 @@
 #include "screen.h"
 #include "space.h"
 #include "sprite.h"
+#include "spritemap.h"
 #include "text.h"
 
 class TitleScreen : public Screen {
@@ -19,10 +20,18 @@ class TitleScreen : public Screen {
   Text text_;
   Space space_;
   Sprite title_;
+  SpriteMap sprites_;
 
   int timer_ = 0;
   int title_timer_ = 0;
 
   bool lets_go = false;
   int fade_timer_ = 0;
+
+  int index = 0;
+  int char_timer_ = 5000;
+
+  int char_sprite() const;
+  std::string char_name() const;
+  std::string char_desc() const;
 };
